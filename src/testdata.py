f3f08fb1a2bdd8e61b149e66f7e159d646ad8d52
Zhang Yiding <2384370688@qq.com>
2018-12-31 20:50:20 +0800

    add all genome test code; add all genome input data

---
+++ src/testdata.py
@@ -0,0 +1,91 @@
+import os
+import sys
+import argparse
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torch.optim as optim
+import random
+import numpy as np
+
+from testdata_dataloader import Cipher_testDataloader
+from network import Network
+from config import DATA_FOLDER #import a variable
+
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', default='simple_network', help='model file definition')
+    parser.add_argument('-bs',default=4, type=int, help='batch size')
+    parser.add_argument('-lt', default=10, type=int, help='Loss file saving refresh interval (seconds)')
+    parser.add_argument('-data_path', default='data/', help='Training path')
+    parser.add_argument('-rundir', default='../results/test' , help='Running directory')
+    parser.add_argument('-ep', default=30, type=int , help='Epochs')
+    parser.add_argument('-start_from', default='../results/1231_4+17_normalized_0.014223675709217787/Best_model_period1.pth' , help='Start from previous model')
+    #parser.add_argument('-start_from', default='', help='Start from previous model')
+    #../results/1125best3_0.019168664837100852/Best_model_period1.pth
+    args = parser.parse_args()
+    return args
+
+
+def get_dataloader(args):
+    # --- dataloader ---
+    full_path = None
+    # arguments check
+    if args.data_path != '':
+        full_path = os.path.join(DATA_FOLDER, args.data_path)
+    # error hint
+    if full_path is None:
+        print("Error: Missing training path for depth!")
+        sys.exit(1)
+    test_data = Cipher_testDataloader(full_path)
+    return test_data
+
+
+
+# main
+if __name__ == '__main__':
+    # --- arguments ---
+    args = parseArgs()
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+
+    # --- dataloader ---
+    try:
+        test_data = get_dataloader(args)
+    except Exception as e:
+        print("Error when get dataloader")
+        sys.exit(1)
+
+    # run path
+    if not os.path.exists(args.rundir):
+        os.mkdir(args.rundir)
+    torch.save(args, args.rundir+'/args.pth')
+
+
+    # --- Model and criterion ---
+    # model
+    config = {}
+    print(args.start_from)
+    model = torch.load(args.start_from)
+    config = model.config
+    model = model.to(device=device)
+    test_loader = DataLoader(test_data, 1, shuffle=False)
+
+
+    it_num = 0
+    result = []
+    lfile = open(args.rundir + '/test_result.txt', 'w')
+    for test_input in test_loader:
+        it_num += 1
+        test_input = test_input.to(device)
+        test_output = model(test_input)
+        test_output.detach()
+        lfile.write('order {}, test output = {}\n'.format(it_num, test_output))
+        #print("order=",it_num,test_output)
+        result.append(test_output)
+    print(it_num)
+
+
+
+
+
---
+++ src/testdata_dataloader.py
@@ -0,0 +1,84 @@
+import os
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+import random
+
+class Cipher_testDataloader(Dataset):
+    def __init__(self, data_path):
+        self.data_path = data_path
+        #read unlabeled test data and all data
+        self.data, self.all_data = self.read_data(data_path)
+        #change inf of GO to max value of GO in self.all_data
+        self.convert_inf(4,21)
+        # calculate mean and std for self.all_data
+        self.mean, self.std = self.calculate_mean_std()
+        self.data = torch.tensor(self.data)
+
+
+    def read_data(self, data_path):
+        all_unlabeled_file = os.path.join(data_path, "input_all_log.txt")
+        #print(positive_file)
+        all_file = os.path.join(data_path, "input_all_with_labeled_data_log.txt")
+        # unlabeled list
+        all_unlabeled_list = []
+        with open(all_unlabeled_file, "r") as f:
+            content = f.read().split("\n")
+            for l in content:
+                all_unlabeled_list.append([float(i) for i in l.split("\t")[2:]])
+        all_list = []
+        with open(all_file, "r") as f:
+            content = f.read().split("\n")
+            for l in content:
+                all_list.append([float(i) for i in l.split("\t")[2:]])
+        all_unlabeled_list = np.array(all_unlabeled_list, dtype=np.float32)
+        all_list = np.array(all_list, dtype=np.float32)
+        return all_unlabeled_list, all_list
+
+    def convert_inf(self,start,end):
+        if end <= start:
+            print("warning: Please correct your input index")
+            return
+        # change Inf of GO to Max value of GO
+        inf_ind = np.where(self.all_data[:, start:end] == float("inf"))
+        self.all_data[:, start:end][inf_ind] = -1
+        max_GO = np.max(self.all_data[:, start:end])
+        self.all_data[:, start:end][inf_ind] = max_GO
+
+
+    def calculate_mean_std(self):
+        #use all_list to get mean and std
+        mean_data = np.mean(self.all_data, axis=0)
+        std_data = np.std(self.all_data, axis=0)
+        return torch.tensor(mean_data, dtype=torch.float), \
+               torch.tensor(std_data, dtype=torch.float)
+
+    def transform(self, raw_data):
+        #self.mean_use = np.delete(self.mean, self.del_col, 0)
+        #self.std_use = np.delete(self.std, self.del_col, 0)
+        #return (raw_data - self.mean_use)/self.std_use
+        return (raw_data - self.mean) / self.std
+        #return raw_data
+
+    def __len__(self):
+        return len(self.data)
+
+
+    def __getitem__(self, index):
+        data_in = self.data[index % len(self.data)]
+        normalized_in = self.transform(data_in)
+        return normalized_in
+
+
+if __name__ == '__main__':
+    test_dataset = Cipher_testDataloader("../data/")
+    test_loader = DataLoader(dataset=test_dataset, batch_size=1, shuffle=False)
+    num_iter = 0
+    for input in test_loader:
+         num_iter += 1
+         print(input)
+         print(input.size())
+
+         #print(neg_input)
+    print(num_iter)
+
